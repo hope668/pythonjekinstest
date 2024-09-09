9ee78ca26051dff6cc5c76dc599c40c718b87fb8
huangtengjie <huangtengjie@kuaishou.com>
2024-09-09 15:22:47 +0800

    新文件：   main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "https://open.feishu.cn/open-apis/bot/v2/hook/72849d75-820d-4189-9f57-a5fec7d2ea2d"
+payload = json.dumps({
+	"content": "{\"text\":\"test content\"}",
+	"msg_type": "text",
+	"receive_id": "ou_7d8a6e6df7621556ce0d21922b676706ccs",
+	"uuid": "选填，每次调用前请更换，如a0d69e20-1dd1-458b-k525-dfeca4015204"
+})
+
+
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response.text)
