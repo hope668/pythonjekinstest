1dce9cd2607a5222ecb2ddcdadb6ba2141ca1956
huangtengjie <huangtengjie@kuaishou.com>
2024-09-09 18:02:39 +0800

    更新时间显示

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import time
+
 import requests
 import json
 url = "https://open.feishu.cn/open-apis/bot/v2/hook/72849d75-820d-4189-9f57-a5fec7d2ea2d"
-payload = json.dumps({
-	"content": "{\"text\":\"test content\"}",
-	"msg_type": "text",
-	"receive_id": "ou_7d8a6e6df7621556ce0d21922b676706ccs",
-	"uuid": "选填，每次调用前请更换，如a0d69e20-1dd1-458b-k525-dfeca4015204"
+# payload = json.dumps({
+# 	"content": "{\"text\":\"test content\"}",
+# 	"msg_type": "text",
+# 	"receive_id": "ou_7d8a6e6df7621556ce0d21922b676706ccs",
+# 	"uuid": "选填，每次调用前请更换，如a0d69e20-1dd1-458b-k525-dfeca4015204"
+# })
+payload=json.dumps({
+        "timestamp": "1599360473",
+        "sign": "",
+        "msg_type": "text",
+        "content": {
+                "text": f'request example--{time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())}'
+        }
 })
 
-
+print(time.time())
 headers = {
   'Content-Type': 'application/json'
 }
